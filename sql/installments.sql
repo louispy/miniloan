9344d55f91118db375841b2446d07a6a4062a08a
louispy <louisleslie98@gmail.com>
2026-05-09 15:16:22 +0700

    Add index on installments

--- sql/installments.sql
+++ sql/installments.sql
@@ -11,3 +11,6 @@ CREATE TABLE installments (
     created_at TIMESTAMP NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
+
+CREATE INDEX idx_installments_loan_status_duedate
+    ON installments (loan_id, status, due_date);
